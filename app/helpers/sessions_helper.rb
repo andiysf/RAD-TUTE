bdc49b318b28f95849b4a00d8302c2a5d1d4d569
user <nicolaslee888@yahoo.com>
2020-05-06 22:04:29 +1000

    tute7

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,22 +1,6 @@
 module SessionsHelper
     def log_in(user)
-        session[:user_id] = user.id
-    end
-
-    def current_user
-        if session[:user_id]
-            @current_user ||= User.find_by(id: session[:user_id])
-        end
-    end
-
-    def logged_in?
-        !current_user.nil?
-    end
-
-    def log_out
-        forget(current_user)
-        session.delete(:user_id)
-        @current_user = nil
+        session[:userid] = user.id
     end
 
     def remember(user)
@@ -26,10 +10,9 @@ module SessionsHelper
     end
 
     def current_user
-        if (user_id = session[:user_id])
-            @current_user ||= User.find_by(id: user_id)
+        if session[:userid]
+            @current_user ||= User.find_by(id: session[:user_id])
         elsif (user_id = cookies.signed[:user_id])
-            #raise
             user = User.find_by(id: user_id)
             if user && user.authenticated?(cookies[:remember_token])
                 log_in user
@@ -38,9 +21,19 @@ module SessionsHelper
         end
     end
 
+    def logged_in?
+        !current_user.nil?
+    end
+
     def forget(user)
         user.forget
         cookies.delete(:user_id)
-        cookies.delete(:remember_token)
+        cookies.delete(:remember_token)    
+    end
+
+    def log_out
+        forget(current_user)
+        session.delete(:user_id)
+        @current_user = nil
     end
 end
